d7ff2e9d107432a2610c6b45deced9f9423cf92c
Danish-H <me@danishhumair.com>
2013-03-17 10:07:00 +0500

    Added files

---
+++ Dingo X OS/Kernel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace Dingo_X_OS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Welcome to the Dingo X OS!");
+        }
+
+        protected override void Run()
+        {
+        start:
+            Console.WriteLine("User: ");
+            string command = Console.ReadLine();
+            if (command == "/echo.on") goto echo;
+            if (command == "/help") Console.WriteLine("Type in echo to turn on echo mode.");
+            if (command == "cls") goto cls;
+            else
+                Console.WriteLine("Incorrect Command.");
+            goto start;
+        echo:
+            Console.WriteLine("Echo: ");
+            string echo = Console.ReadLine();
+            if (echo == "echo.off") goto start;
+            else
+            Console.WriteLine("Echo: " + echo);
+            goto echo;
+        cls:
+            Console.Clear();
+            goto start;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dingo X OS
+One of my early projects from back when I was 10, a simple operating system with revolutionary features like 'echo' and 'help', written in C# using the Cosmos 'construction kit'.
